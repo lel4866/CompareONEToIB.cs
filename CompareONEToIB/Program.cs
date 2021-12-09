9a49a0e57bd879cdd1986eb765f5d02bf05502d3
Larry Lewis <lel486@gmail.com>
2021-12-09 16:02:22 -0500

    initial commit

--- CompareONEToIB/Program.cs
+++ CompareONEToIB/Program.cs
@@ -1,2 +1,57 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+
+static class Program
+{
+    public const string version = "0.0.1";
+    public const string version_date = "2021-12-08";
+
+    static int Main(string[] args)
+    {
+        var stopWatch = new Stopwatch();
+        stopWatch.Start();
+
+        bool rc = ProcessCommandLineArguments(args); // calls System.Environment.Exit(-1) if bad command line arguments
+        if (!rc)
+            return -1;
+
+        rc = ReadONEData();
+        if (!rc)
+            return -1;
+
+        rc = ReadIBData();
+        if (!rc)
+            return -1;
+
+        rc = CompareONEToIB();
+        if (!rc)
+            return -1;
+
+        stopWatch.Stop();
+        Console.WriteLine($"Elapsed time = {stopWatch.Elapsed}");
+
+        return 0;
+    }
+
+    static bool ProcessCommandLineArguments(string[] args)
+    {
+        return true;
+    }
+
+    static bool ReadONEData()
+    {
+        return true;
+    }
+
+    static bool ReadIBData()
+    {
+        return true;
+    }
+
+    static bool CompareONEToIB()
+    {
+        return true;
+    }
+
+}
