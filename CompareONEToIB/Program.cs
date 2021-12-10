679ec5e87eacad9111db23cd6eadf8805f27223c
Larry Lewis <lel486@gmail.com>
2021-12-09 21:27:15 -0500

    add code...

--- CompareONEToIB/Program.cs
+++ CompareONEToIB/Program.cs
@@ -6,7 +6,7 @@ static class Program
 {
     public const string version = "0.0.1";
     public const string version_date = "2021-12-08";
-    public const string ib_directory = @"C: \Users\lel48\OneDrive\Documents\IBExport\";
+    public const string ib_directory = @"C:\Users\lel48\OneDrive\Documents\IBExport\";
     public const string one_directory = @"C:\Users\lel48\OneDrive\Documents\ONEExport\";
 
 
@@ -80,6 +80,36 @@ static class Program
 
     static bool ReadIBData()
     {
+        const string filename_pattern = "*.csv";
+        string[] files;
+        if (Directory.Exists(one_directory))
+        {
+            files = Directory.GetFiles(one_directory, filename_pattern, SearchOption.TopDirectoryOnly);
+            bool file_found = false;
+            foreach (string full_fn in files)
+            {
+                string filename = Path.GetFileName(full_fn);
+                string datestr = filename.Substring("portfolio.".Length, filename_pattern.Length - "portfolio..csv".Length);
+                if (DateTime.TryParse(datestr, out DateTime dateValue))
+                {
+                    file_found = true;
+                }
+                else
+                {
+                    continue;
+                }
+            }
+            if (!file_found)
+            {
+                Console.WriteLine("***Error*** No OptionNet files found");
+                return false;
+            }
+        }
+        else
+        {
+            return false;
+        }
+
         return true;
     }
 
